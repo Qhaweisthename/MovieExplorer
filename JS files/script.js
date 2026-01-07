6497644152f94588da590dd68525c3917a976e7e
Qhawe Ngcongo <ST10283124@vcconnect.edu.za>
2026-01-07 20:59:30 +0200

    initial commit

---
+++ JS files/script.js
@@ -0,0 +1,19 @@
+
+//this was fetch
+
+// const apiKey = 'f682a15a0e45859d8acb76035c02813b';
+// const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=London`;
+
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => console.error(err));
+
+
+
+
+
+
+
